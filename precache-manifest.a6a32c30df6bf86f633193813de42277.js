c832de6ee3e35141b13cac7ee3d75c9951f7a885
mustafa <mustafa.ahmad@3lmdigital.com>
2020-03-08 09:20:43 +0200

    Updates

---
+++ precache-manifest.a6a32c30df6bf86f633193813de42277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ab702ad29b5cbdc63315e03ce9f171",
+    "url": "/cart-app/index.html"
+  },
+  {
+    "revision": "362d6d7ba6a1f420538b",
+    "url": "/cart-app/static/css/main.245a317b.chunk.css"
+  },
+  {
+    "revision": "47e54a6af7842aacd501",
+    "url": "/cart-app/static/js/2.3b6153bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cart-app/static/js/2.3b6153bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362d6d7ba6a1f420538b",
+    "url": "/cart-app/static/js/main.feaafb62.chunk.js"
+  },
+  {
+    "revision": "87af2c910b80410cfe7f",
+    "url": "/cart-app/static/js/runtime-main.5c3518af.js"
+  }
+]);
